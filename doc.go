dd198da6ea4940e9c8b2a1ee3161fcba5a322e70
Vitor Leal <vitorleal1@gmail.com>
2015-05-04 16:12:15 -0300

    add content in the doc

--- doc.go
+++ doc.go
@@ -1,1 +1,23 @@
+// Package find-cine to search for movie theaters close to an address.
+//
+// Example:
+//  import (
+//    "fmt"
+//    "github.com/vitorleal/find-cine-go"
+//  )
+//
+//  func main() {
+//    findcine = FindCine{}
+//
+//    theaters, err := findcine.Near("Miami")
+//
+//    if err != nil {
+//      fmt.Println(err)
+//    }
+//
+//    fmt.Println(theaters)
+//  }
+//
+
 package findcine
+
